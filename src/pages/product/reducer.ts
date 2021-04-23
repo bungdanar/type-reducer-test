9d01c04d4cf5116e473c4e1a9989f363151f3820
Bung Danar <danar.g.putera@gmail.com>
2021-04-23 08:55:32 +0700

    Updated type in reducer

--- src/pages/product/reducer.ts
+++ src/pages/product/reducer.ts
@@ -1,6 +1,40 @@
-export const productReducer = (state: any, action: any) => {
+type ActionMap<M extends { [index: string]: any }> = {
+  [Key in keyof M]: M[Key] extends undefined
+    ? { type: Key }
+    : { type: Key; payload: M[Key] }
+}
+
+export enum Types {
+  Create = 'CREATE_PRODUCT',
+  Delete = 'DELETE_PRODUCT',
+  Add = 'ADD_PRODUCT',
+}
+
+type ProductType = {
+  id: number
+  name: string
+  price: number
+}
+
+type ProductPayload = {
+  [Types.Create]: {
+    id: number
+    name: string
+    price: number
+  }
+  [Types.Delete]: {
+    id: number
+  }
+}
+
+export type ProductActions = ActionMap<ProductPayload>[keyof ActionMap<ProductPayload>]
+
+export const productReducer = (
+  state: ProductType[],
+  action: ProductActions | ShoppingCartActions
+) => {
   switch (action.type) {
-    case 'CREATE_PRODUCT': {
+    case Types.Create: {
       return [
         ...state,
         {
@@ -11,12 +45,8 @@ export const productReducer = (state: any, action: any) => {
       ]
     }
 
-    case 'DELETE_PRODUCT': {
-      return [
-        ...state.filter(
-          (product: { id: any }) => product.id !== action.payload.id
-        ),
-      ]
+    case Types.Delete: {
+      return [...state.filter((product) => product.id !== action.payload.id)]
     }
 
     default:
@@ -24,9 +54,18 @@ export const productReducer = (state: any, action: any) => {
   }
 }
 
-export const shoppingCartReducer = (state: any, action: any) => {
+type ShoppingCartPayload = {
+  [Types.Add]: undefined
+}
+
+export type ShoppingCartActions = ActionMap<ShoppingCartPayload>[keyof ActionMap<ShoppingCartPayload>]
+
+export const shoppingCartReducer = (
+  state: number,
+  action: ProductActions | ShoppingCartActions
+) => {
   switch (action.type) {
-    case 'ADD_PRODUCT': {
+    case Types.Add: {
       return state + 1
     }
 
